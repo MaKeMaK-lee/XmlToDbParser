2cc0eab5db482655234ed59e72f67463371e3a98
MaKeMaK <73843102+MaKeMaK-lee@users.noreply.github.com>
2024-09-17 18:06:29 +0300

    UI created

--- XmlToDbParser/Program.cs
+++ XmlToDbParser/Program.cs
@@ -6,15 +6,40 @@ namespace XmlToDbParser
     {
         static void Main(string[] args)
         {
-            using XmlToDbParserDatabase database = new("XmlToDbParserDatabase_1.db");
+            while (true)
+            {
+                using XmlToDbParserDatabase database = new("XmlToDbParserDatabase_1.db");
 
-            var xmlOrders = XmlOrdersParser.Parse("example.xml");
-            if (xmlOrders == null)
-                return;
+                Console.WriteLine();
+                Console.WriteLine();
 
-            xmlOrders.AddOrUpdateTo(database);
+                try
+                {
+                    Console.WriteLine("Введите имя файла:");
+                    string? filePath = Console.ReadLine();
 
+                    var xmlOrders = XmlOrdersParser.Parse(filePath!);
+                    if (xmlOrders == null)
+                        return;
 
+                    xmlOrders.AddOrUpdateTo(database);
+
+                    Console.WriteLine("Изменения в базу внесены.");
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("Ошибка: не удалось найти указанный файл.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ошибка:\n" + e.Message);
+                }
+
+                Console.WriteLine("Нажмите любую клавишу для завершения работы или C для смены имени файла.");
+                if (Console.ReadKey(true).Key == ConsoleKey.C)
+                    continue;
+                break;
+            }
         }
     }
 }
--- XmlToDbParser/XmlOrdersParser.cs
+++ XmlToDbParser/XmlOrdersParser.cs
@@ -6,8 +6,10 @@ namespace XmlToDbParser
     {
         public static Entities.Xml.orders? Parse(string filePath)
         {
+            ArgumentNullException.ThrowIfNull(filePath);
+
             XmlSerializer xmlSerializer = new(typeof(Entities.Xml.orders));
-            using FileStream fs = new(filePath, FileMode.OpenOrCreate);
+            using FileStream fs = new(filePath, FileMode.Open);
             return xmlSerializer.Deserialize(fs) as Entities.Xml.orders;
         }
     }
